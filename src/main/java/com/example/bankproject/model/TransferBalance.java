f46cd5e5e56a704272159fb7392bb87bfef34802
Daniiar Satybaldiev <82278620+dsatybaldiev86@users.noreply.github.com>
2022-08-10 12:52:45 +0700

    Commit 10.08.22

---
+++ src/main/java/com/example/bankproject/model/TransferBalance.java
@@ -0,0 +1,15 @@
+package com.example.bankproject.model;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransferBalance {
+
+    private Long from;
+    private Long to;
+    private BigDecimal amount;
+
+}
